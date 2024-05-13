7cba2118b2d9009c2ca366bff31e46086e027e5e
selena02 <selenahurloi27@gmail.com>
2024-05-13 13:16:46 +0300

    Get users followers added

---
+++ server/src/Application/Users/Queries/GetUserFollowers/GetUserFollowers.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Interfaces;
+using Application.Common.Models;
+
+namespace Application.Users.Queries.GetUserFollowers;
+
+public record GetUserFollowersQuery(int UserId) : IRequest<GetUserFollowersResponse>;
+
+public record GetUserFollowersResponse(List<FollowerDto> Followers);
+
+public class GetUserFollowersQueryHandler : IRequestHandler<GetUserFollowersQuery, GetUserFollowersResponse>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetUserFollowersQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<GetUserFollowersResponse> Handle(GetUserFollowersQuery request, CancellationToken cancellationToken)
+    {
+        var followers = await _context.Follows
+            .Where(f => f.FollowerId == request.UserId)
+            .Select(f => new FollowerDto(f.FollowerId, f.Follower.UserName, f.Follower.ProfilePictureUrl))
+            .ToListAsync(cancellationToken);
+
+        return new GetUserFollowersResponse(followers);
+    }
+}
---
+++ server/src/Application/Users/Queries/GetUserFollowers/GetUserFollowersQueryValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Users.Queries.GetUserFollowers;
+
+public class GetUserFollowersQueryValidator : AbstractValidator<GetUserFollowersQuery>
+{
+    public GetUserFollowersQueryValidator()
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage("User id required");
+    }
+}
